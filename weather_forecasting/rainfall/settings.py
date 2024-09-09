bdb229d251080e75f76c2fdcd6053b83b74ec339
RJW20 <robertjwatts84@outlook.com>
2024-09-09 09:34:53 +0100

    Change rainfall and temperature dataset proportions

--- weather_forecasting/rainfall/settings.py
+++ weather_forecasting/rainfall/settings.py
@@ -3,8 +3,8 @@
 settings = {
 
     'data_location': "weather_data/2017_2023.csv",
-    'train_prop': 0.5,
-    'val_prop': 0.25,
+    'train_prop': 0.7,
+    'val_prop': 0.2,
     'sampling_rate': 1,
     'window_size': 120,
     'batch_size': 256,
