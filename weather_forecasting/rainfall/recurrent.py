af486176a80c4ce8391c48140eec2392ca8cd5d9
RJW20 <robertjwatts84@outlook.com>
2024-09-12 10:18:18 +0100

    Implement figure saving for all rainfall models

--- weather_forecasting/rainfall/recurrent.py
+++ weather_forecasting/rainfall/recurrent.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers
 
@@ -11,8 +12,11 @@ def simple_recurrent() -> None:
     """Implements a simple recurrent neural network for rainfall prediction.
     
     Saves the final model to models/rainfall/recurrent.keras
-    Prints the MAE on the validation datasets.
-    Displays the loss curves for validation and training.
+    Displays the loss curves for validation and training MAE, and saves
+    the plot to figures/rainfall/recurrent_training.png.
+    Displays a sample of the predictions against their targets for the test
+    dataset, and saves the plot to figures/rainfall/recurrent_evaluation.png.
+    Prints the MAE on the test dataset.
     """
 
     train_dataset, val_dataset, test_dataset = load_data(
@@ -28,8 +32,11 @@ def simple_recurrent() -> None:
 
     save_location = "models/rainfall/recurrent.keras"
     train_model(model, train_dataset, val_dataset, save_location)
+    plt.savefig("figures/rainfall/recurrent_training.png", bbox_inches="tight")
     model = keras.models.load_model(save_location)
     evaluate_model(model, test_dataset, "rain (mm)")
+    plt.savefig("figures/rainfall/recurrent_evaluation.png", bbox_inches="tight")
+    plt.show()
 
 
 if __name__ == "__main__":
